c1f24792a3754dae4927797bde022e1d9427f0a0
priya <priyankatathe4@gmail.com>
2024-06-28 12:49:56 +0530

    modal update

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name : { type: String, required: true },
     desc: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true },
